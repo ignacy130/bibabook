033e021357e71426b2899e3201d81ee8df861e26
ignacy130 <ignacy130@gmail.com>
2015-03-26 16:47:41 +0100

    cleanup cd

---
+++ Bibabook/Bibabook.Implementation/AppEventService/EventsService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contract;
+namespace Bibabook.Implementation.AppEventService
+{
+    /// <summary>
+    /// Odpowiedzialny za obsluge (dodawanie, edycje, dodawanie uczestnikow) wydarzen
+    /// </summary>
+    public class EventsService :IAppEventService
+    {
+        public bool Create(IAppEvent appEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Cancel(IAppEvent appEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(IAppEvent appEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InviteUser(IAppEvent appEvent, IAppUser sender, IAppUser recipient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InviteUser(IAppUser appEvent, IAppUser sender, ICollection<IAppUser> recipients)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EnrollUser(IAppUser appEvent, IAppUser appUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveUser(IAppEvent appEvent, IAppUser appUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveUser(IAppEvent appEvent, ICollection<IAppUser> appUsers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ForbidUser(IAppEvent appEvent, IAppUser appUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AllowUser(IAppEvent appEvent, IAppUser appUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddEventPost(IAppEvent appEvent, IEventPost eventPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
