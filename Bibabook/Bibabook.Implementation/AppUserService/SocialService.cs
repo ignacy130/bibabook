9052d582cda618e3b766400922cd49a69903a7b5
ignacy130 <ignacy130@gmail.com>
2015-03-26 16:49:03 +0100

    even more cleanup wtf

---
+++ Bibabook/Bibabook.Implementation/AppUserService/SocialService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contract;
+
+namespace Bibabook.Implementation.AppUserService
+{
+    /// <summary>
+    /// Odpowiedzialny za wysylanie zaproszen do grona znajomych oraz dodawanie i usuwanie znajomych
+    /// </summary>
+    public class SocialService : IAppUserSocialService
+    {
+        public bool SendFriendInvitation(IAppUser sender, IAppUser receiver)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FriendUsers(IAppUser firstUser, IAppUser secondUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UnfriendUsers(IAppUser firstUser, IAppUser secondUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bibabook/Bibabook.Implementation/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Contract;
+using Contract.Enums;
+
+namespace Bibabook.Models
+{
+    public class AppUser : Entity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public Credentials Credentials { get; set; } 
+        public DateTime Birthday { get; set; }
+        public Sex Sex { get; set; }
+        public virtual ICollection<AppUser> Friends { get; set; }
+        public virtual ICollection<Notification> Notifications { get; set; }
+        public virtual ICollection<AppEvent> Events { get; set; }
+        public virtual ICollection<IEventPost> Posts { get; set; }
+    }
+}
