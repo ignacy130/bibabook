6193f4518a4bb3532c15c65c6e25d6a84d60bbed
Mateusz Chodkowski <chotkos@gmail.com>
2015-04-26 23:05:51 +0200

    SocialService, wstępna implementacja.

--- Bibabook/Bibabook.Implementation/AppUserService/SocialService.cs
+++ Bibabook/Bibabook.Implementation/AppUserService/SocialService.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Contract;
+using Bibabook.Implementation.Models;
+using Bibabook.Implementation.DatabaseContext;
+using System.Data.Entity;
 
 namespace Bibabook.Implementation.AppUserService
 {
@@ -12,19 +15,106 @@ namespace Bibabook.Implementation.AppUserService
     /// </summary>
     public class SocialService : IAppUserSocialService
     {
+        INotificationService _notificationService;
+        DataBaseContext _context;
+        public SocialService(INotificationService notificationService)
+        {
+            _context = new DataBaseContext();
+            _notificationService = notificationService;
+        }
+
         public bool SendFriendInvitation(IAppUser sender, IAppUser receiver)
         {
-            throw new NotImplementedException();
+            
+            FriendInvitation fi = new FriendInvitation()
+            {
+                Created = DateTime.Now,
+                Deleted = false,
+                EntityID = new Guid(),
+                FriendInvitationID = new Guid(),
+                Modified = DateTime.Now,
+                Recipient = (AppUser)receiver,
+                Sender = (AppUser)sender
+            };
+            _context.FriendInvatations.Add(fi);
+            
+            Notification n = new Notification()
+            {
+                Created = DateTime.Now,
+                Deleted = false,
+                EntityID = new Guid(),
+                Json = "",
+                Modified = DateTime.Now,
+                NotificationID = new Guid(),
+                Receivied = false
+            };
+            
+            var au = (AppUser)receiver;
+            _notificationService.Create(n);
+
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
         }
 
         public bool FriendUsers(IAppUser firstUser, IAppUser secondUser)
         {
-            throw new NotImplementedException();
+            var fu = (AppUser)firstUser;
+            var su = (AppUser)secondUser;
+
+            su.Friends.Add(su);
+            su.Friends.Add(fu);
+            _context.Entry(fu).State = EntityState.Modified;
+            _context.Entry(su).State = EntityState.Modified;
+            
+            if (_context.FriendInvatations.Any(x => (x.Sender.EntityID == su.EntityID && x.Recipient.EntityID == fu.EntityID) || ( x.Sender.EntityID == fu.EntityID && x.Recipient.EntityID == su.EntityID)))
+            {
+                var invs = _context.FriendInvatations.Where(x => (x.Sender.EntityID == su.EntityID && x.Recipient.EntityID == fu.EntityID) || (x.Sender.EntityID == fu.EntityID && x.Recipient.EntityID == su.EntityID));
+                foreach (FriendInvitation invit in invs)
+                {
+                    invit.Deleted = true;
+                    invit.Modified = DateTime.Now;
+                    _context.Entry(invit).State = EntityState.Modified;
+                }
+            }
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
         }
 
         public bool UnfriendUsers(IAppUser firstUser, IAppUser secondUser)
         {
-            throw new NotImplementedException();
+            var fu = (AppUser)firstUser;
+            var su = (AppUser)secondUser;
+
+            su.Friends.Remove(su);
+            su.Friends.Remove(fu);
+            _context.Entry(fu).State = EntityState.Modified;
+            _context.Entry(su).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
     }
 }
