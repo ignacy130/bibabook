08ed4db8db6d49e109f9e52de6f3af9166099271
unknown <ignacy130@gmail.com>
2015-05-06 09:25:24 +0200

    service injection example

--- Bibabook/Bibabook/Controllers/HomeController.cs
+++ Bibabook/Bibabook/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Bibabook.Implementation.AppUserService;
+using Contract;
+using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,22 +11,28 @@ namespace Bibabook.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        IAppUserSocialService _socialService;
+        IKernel container = Container.Configuration.CONTAINER;
+        
+        public ActionResult Index(IAppUserSocialService socialService)
         {
+            IAppUserSocialService aa = container.Get<IAppUserSocialService>();
+            //IAppUser bb = new AppUser();
+            //aa.SendFriendInvitation();
+            _socialService = socialService;
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            _socialService.
             return View();
         }
     }
