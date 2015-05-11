e123a0ab74641620262a7dbba93fdf851434ce9d
rogal93 <damian.rogal@gmail.com>
2015-05-11 21:09:27 +0200

    Implementacja klasy EmailService. Jak cos źle to krzyczeć na Damiana.

--- Bibabook/Bibabook.Implementation/EmailService/EmailService.cs
+++ Bibabook/Bibabook.Implementation/EmailService/EmailService.cs
@@ -4,36 +4,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Contract;
+using Bibabook.Implementation.Models;
 
 namespace Bibabook.Implementation.EmailService
 {
     public class EmailService : IEmailService
     {
+        private System.Net.Mail.SmtpClient client;
+
         public System.Net.Mail.SmtpClient Client
         {
             get
             {
-                throw new NotImplementedException();
+                return client;
             }
             set
             {
-                throw new NotImplementedException();
+                client = value;
             }
         }
 
+        public EmailService() { }
+
+
         public IEmail CreateEmail(string receiverEmailAddress, string subject, string message)
         {
-            throw new NotImplementedException();
+            IEmail email = new Email()
+            {
+                Content = message,
+                Date = DateTime.Now,
+                EmailID = Guid.NewGuid(),
+                RecipientEmail = receiverEmailAddress,
+                Subject = subject
+            };
+            return email;
         }
 
         public bool SendEmail(IEmail email)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Client.Send("Bibabook", email.RecipientEmail, email.Subject, email.Content);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public bool SendEmail(ICollection<string> receiversEmailAddresses, string subject, string message)
         {
-            throw new NotImplementedException();
+            List<string> receivers = receiversEmailAddresses.ToList();
+            bool result = false;
+
+            foreach (string receiver in receivers)
+            {
+                Email email = new Email()
+                {
+                    Content = message,
+                    Subject = subject,
+                    Date = DateTime.Now,
+                    EmailID = Guid.NewGuid(),
+                    RecipientEmail = receiver
+                };
+                result = SendEmail(email);
+            }
+            return result;
         }
     }
 }
