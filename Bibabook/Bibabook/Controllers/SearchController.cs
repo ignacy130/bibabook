187cf0af8c170f1e761c57bf25b86b779d3f1085
unknown <ignacy130@gmail.com>
2015-06-19 12:49:56 +0200

    multitype search working

---
+++ Bibabook/Bibabook/Controllers/SearchController.cs
@@ -0,0 +1,54 @@
+﻿using Bibabook.DAL;
+using Bibabook.Filters;
+using Bibabook.Implementation.DatabaseContext;
+using Bibabook.Models;
+using Contract;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bibabook.Controllers
+{
+    public class SearchController : Controller
+    {
+        DataBaseContext db = new DataBaseContext();
+
+        IAppUserService _usersService;
+        IAppUserSocialService _socialService;
+        IKernel container = Container.Configuration.CONTAINER;
+
+        // GET: Search
+        [LoggedFilter]
+        public ActionResult Search(string name)
+        {
+            var logged = UserHelper.GetLogged(Session);
+            var users = db.AppUsers.Where(x => x.Name.Contains(name) || x.Surname.Contains(name)).Where(x => x.Email != logged.Email).ToList();
+            var events = db.AppEvents.Where(x => x.Name.Contains(name) || x.Description.Contains(name)).ToList();
+            var results = new List<SearchItemViewModel>();
+            foreach (var item in users)
+            {
+                results.Add(
+                    new SearchItemViewModel()
+                    {
+                        Id = item.AppUserID,
+                        Name = item.Name + " " + item.Surname,
+                        Type = AppType.AppUser
+                    });
+            }
+            foreach (var item in events)
+            {
+                results.Add(
+                    new SearchItemViewModel()
+                    {
+                        Id = item.AppEventID,
+                        Name = item.Name,
+                        Type = AppType.AppEvent
+                    });
+            }
+            return PartialView("Results", results);
+        }
+    }
+}
---
+++ Bibabook/Bibabook/Models/SearchItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bibabook.Models
+{
+    public enum AppType
+    {
+        AppEvent,
+        AppUser
+    }
+
+    public class SearchItemViewModel
+    {
+        public AppType Type { get; set; }
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+    }
+}
