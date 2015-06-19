6a2827d09964a75221b09f248d8968f718586dd8
unknown <ignacy130@gmail.com>
2015-06-19 12:49:06 +0200

    appevent faking viewmodel

--- Bibabook/Bibabook.Implementation/Models/AppEvent.cs
+++ Bibabook/Bibabook.Implementation/Models/AppEvent.cs
@@ -4,23 +4,47 @@ using System.Linq;
 using System.Web;
 using Contract;
 using Contract.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bibabook.Implementation.Models
 {
     public class AppEvent : Entity, IAppEvent
     {
         public Guid AppEventID { get; set; }
+
+        [DisplayName("Imię")]
+        [Required(ErrorMessage = "Pole jest wymagane")]
         public string Name { get; set; }
+
+        [DisplayName("Opis")]
+        [Required(ErrorMessage = "Pole jest wymagane")]
         public string Description { get; set; }
+
+        [DisplayName("Tylko dorośli")]
         public bool AdultsOnly { get; set; }
+
+        [DisplayName("Data początku")]
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage = "Pole jest wymagane")]
         public DateTime TimeStart { get; set; }
+
+        [DisplayName("Data końca")]
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage = "Pole jest wymagane")]
         public DateTime TimeEnd { get; set; }
-        public AppUser Host { get; set; }
+
+        [DisplayName("Gospodarz")]
+        public virtual AppUser Host { get; set; }
         public virtual List<AppUser> Guests { get; set; }
         public virtual List<EventPost> Posts { get; set; }
+
+        [DisplayName("Cena za wejście")]
         public Decimal EntryFee { get; set; }
         public bool IsActive { get; set; }
         public Privacy Privacy { get; set; }
+
+        [DisplayName("Tło")]
         public string Background { get; set; }
     }
 }
